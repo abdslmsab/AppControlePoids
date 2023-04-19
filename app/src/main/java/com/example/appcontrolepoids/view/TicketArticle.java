5d57fab5325f3da978f1d08278d28ce4d186c633
Sabri ABDESSELEM <sabri.abdesselem@etu.iut-tlse3.fr>
2023-04-19 22:59:04 +0200

    title deleted from TicketArticle

--- app/src/main/java/com/example/appcontrolepoids/view/TicketArticle.java
+++ app/src/main/java/com/example/appcontrolepoids/view/TicketArticle.java
@@ -1,6 +1,18 @@
 package com.example.appcontrolepoids.view;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.appcontrolepoids.R;
+
+import java.util.Objects;
+
 public class TicketArticle extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_article);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+    }
 }
