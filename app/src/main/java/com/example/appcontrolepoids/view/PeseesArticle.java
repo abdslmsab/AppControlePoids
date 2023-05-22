77f7352b64cf8fd309bd5ab4fcddc57454370bd4
Sabri ABDESSELEM <sabri.abdesselem@etu.iut-tlse3.fr>
2023-05-22 17:59:03 +0200

    scale weight

--- app/src/main/java/com/example/appcontrolepoids/view/PeseesArticle.java
+++ app/src/main/java/com/example/appcontrolepoids/view/PeseesArticle.java
@@ -1,9 +1,15 @@
 package com.example.appcontrolepoids.view;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,15 +24,25 @@ import com.example.appcontrolepoids.alertdialog.TypeAlerte;
 import com.example.appcontrolepoids.databinding.ActivityPeseesArticleBinding;
 import com.example.appcontrolepoids.model.Article;
 import com.example.appcontrolepoids.viewmodel.PeseesArticleViewModel;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
 import com.redmadrobot.inputmask.MaskedTextChangedListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Objects;
 
-public class PeseesArticle extends AppCompatActivity implements DialogAlerte.AlertDialogInterface {
+public class PeseesArticle extends AppCompatActivity implements DialogAlerte.AlertDialogInterface, Runnable {
+
+    private static final int PERIOD = 5000;
+    private View root = null;
 
     private PeseesArticleViewModel peseesArticleViewModel;
     private DialogAlerteViewModel dialogAlerteViewModel;
+    private ActivityPeseesArticleBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +51,13 @@ public class PeseesArticle extends AppCompatActivity implements DialogAlerte.Ale
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 
         //Variable de liaison (binding) permettant d'accéder aux éléments de l'interface utilisateur
-        ActivityPeseesArticleBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_pesees_article);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_pesees_article);
         //Initialisation de l'objet PeseesArticleViewModel avec le contexte de l'activité
         peseesArticleViewModel = new ViewModelProvider(this).get(PeseesArticleViewModel.class);
         //Définition de l'objet PeseesArticleViewModel comme variable dans le layout pour le Data Binding
         binding.setPeseesArticleViewModel(peseesArticleViewModel);
         binding.setLifecycleOwner(this);
+        this.root = binding.getRoot();
 
         //Initialisation de l'objet DialogAlerteViewModel avec le contexte de l'activité
         dialogAlerteViewModel = new ViewModelProvider(this).get(DialogAlerteViewModel.class);
@@ -125,6 +142,7 @@ public class PeseesArticle extends AppCompatActivity implements DialogAlerte.Ale
     @Override
     protected void onResume() {
         super.onResume();
+        run();
         peseesArticleViewModel.reinitialiserListePesees();
     }
 
@@ -136,5 +154,69 @@ public class PeseesArticle extends AppCompatActivity implements DialogAlerte.Ale
     @Override
     public void alertDialogAlternativeOption(TypeAlerte type) {
 
+    }
+
+    @Override
+    public void onPause() {
+        root.removeCallbacks(this);
+
+        super.onPause();
+    }
+
+    @Override
+    public void run() {
+        //Partie RS232 vers USB-C
+
+        // Trouve tous les pilotes disponibles à partir des périphériques connectés
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            return;
+        }
+
+        // Ouvre une connexion avec le premier pilote disponible
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            // Ajouter ici la gestion de UsbManager.requestPermission(driver.getDevice(), ..)
+            return;
+        }
+
+        UsbSerialPort port = driver.getPorts().get(0); // La plupart des appareils n'ont qu'un seul port (port 0)
+        try {
+            port.open(connection);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            port.setParameters(9600, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            // Crée un tableau pour stocker les données lues
+            byte[] buffer = new byte[1024];
+            int bytesRead = port.read(buffer, 1000); // Lit les données avec un délai de 1000 millisecondes
+
+            // Convertir les données lues en chaîne
+            String data = new String(buffer, 0, bytesRead);
+
+            // Affichez les données lues
+            Log.d("USB", "Données lues : " + data);
+
+            // Affichez les données de poids dans le champ "poids_brut_edit_text" de la vue "PeseesArticle"
+            binding.poidsBrutEditText.setText(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        root.postDelayed(this, PERIOD);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
